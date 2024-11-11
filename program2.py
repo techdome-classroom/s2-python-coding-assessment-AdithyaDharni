fc81df87e32f9726bbd1df6eef1c4dfcf7e143a6
AdithyaDharni <dharniadithya@gmail.com>
2024-11-11 16:00:00 +0530

    program2.py

--- program2.py
+++ program2.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
 
+        for char in s[::-1]:  # Process from right to left
+            curr_value = roman_to_int[char]
+            if curr_value < prev_value:
+                total -= curr_value
+            else:
+                total += curr_value
+            prev_value = curr_value
 
-
+        return total
